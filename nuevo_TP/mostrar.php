61ff6971f3e60c8e3acd789b2f7de00cb57a79bf
Juan <jmgiusti91@gmail.com>
2016-03-28 19:51:30 -0300

    Tomamos un campo del formulario, lo guardamos en una variable y lo mostramos

--- nuevo_TP/mostrar.php
+++ nuevo_TP/mostrar.php
@@ -1,7 +1,13 @@
 <?php
 	/*echo "<h1>Datos Recibidos</h1>";*/
 	echo "Recibidos: <br>";
-	var_dump($_GET);
+	/*var_dump($_GET);*/
 	echo "<br>";
-	var_dump($_POST);
+	var_dump($_POST); /*La info se pasa en un array de clave valor. */
+	/*Si esta por GET, se almacena en cache, puede guardarlo en favoritos puede recorrer por el historial pero se ve la informacion. Tiene restriccion de longitud. Maximo 2048 caracteres */
+	/* POST no se puede guardar en favoritos, no se puede correr por el historial pero no muestra la informacion ni tiene restriccio de longitud*/
+	$nombre=$_POST["usuario"];
+	echo "<br>";
+	echo "<br>";
+	echo $nombre;	
 ?>
